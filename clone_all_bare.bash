5da06e861c2f91ea31b16013abb67f05e0ec86ba
7Ji <pugokushin@gmail.com>
2023-12-17 22:18:32 +0800

    clone_all_bare: fix on update condition

--- clone_all_bare.bash
+++ clone_all_bare.bash
@@ -2,9 +2,11 @@
 mkdir -p repos
 while read -r line; do
     repo=repos/"${line}".git
-    if [[ "${update_repo}" && -e "${repo}" ]]; then
-        echo "Updating '${line}'"
-        git --git-dir "${repo}" remote update
+    if [[ -e "${repo}" ]]; then
+        if [[ "${update_repo}" ]]; then
+            echo "Updating '${line}'"
+            git --git-dir "${repo}" remote update
+        fi
     else
         echo "Cloning '${line}'"
         rm -rf "${repo}"
